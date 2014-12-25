c75d3a84d83a8d3ca999b21ce542799fd6e78471
lishouhi <houpengfei_2007@163.com>
2014-12-25 09:34:51 +0800

    滑动菜单测试项目(首次签入)

---
+++ src/com/pps/myrenren/activity/AboutFragment.java
@@ -0,0 +1,32 @@
+package com.pps.myrenren.activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	    Bundle savedInstanceState) {
+	View view = inflater.inflate(R.layout.about_fragment, null);
+	return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+	super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+	super.onPause();
+    }
+}
